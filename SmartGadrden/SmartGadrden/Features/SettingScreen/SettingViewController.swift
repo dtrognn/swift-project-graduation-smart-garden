722b03ed0e626cd04b52faefc1df931ceff545af
Duc Trong <trongvd@heligate.com.vn>
2023-04-06 22:21:27 +0700

    set value default var workMode

--- SmartGadrden/SmartGadrden/Features/SettingScreen/SettingViewController.swift
+++ SmartGadrden/SmartGadrden/Features/SettingScreen/SettingViewController.swift
@@ -21,12 +21,14 @@ class SettingViewController: BaseViewController {
     @IBOutlet var thresholdSoilMostureTextField: UITextField!
     @IBOutlet var thresholdLightTextField: UITextField!
 
-    static var workMode: String = ""
+    static var workMode: String = "1"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configValueDefaultSwitch()
         initDataModeFirebase()
+
+        listenSwitchStatusChanged()
     }
 
     private func configValueDefaultSwitch() {
@@ -63,8 +65,72 @@ class SettingViewController: BaseViewController {
 
     private func handleAssignValueSwitch(_ workmode: String) {
         let switches = [buttonControlModeSwitch, appControlModeSwitch, automaticControlModeSwitch]
+
         for (index, control) in switches.enumerated() {
             control?.isOn = ((index + 1) == Int(workmode) ?? switches.count - 1)
         }
+    }
+
+    // MARK: -
+
+    private func listenSwitchStatusChanged() {
+        buttonControlModeSwitch.addTarget(self, action: #selector(switchChanged(_:)), for: .valueChanged)
+        appControlModeSwitch.addTarget(self, action: #selector(switchChanged(_:)), for: .valueChanged)
+        automaticControlModeSwitch.addTarget(self, action: #selector(switchChanged(_:)), for: .valueChanged)
+    }
+
+    @objc func switchChanged(_ sender: UISwitch) {
+        switch sender {
+        case buttonControlModeSwitch:
+            appControlModeSwitch.isOn = false
+            automaticControlModeSwitch.isOn = false
+
+            writeDataToFirebase("CHEDO", "1") { [weak self] result in
+                switch result {
+                case .success:
+                    self?.handleWriteDataSuccessed(sender.tag)
+                case .failure(let error):
+                    self?.handleWriteDataFailed(error)
+                }
+            }
+        case appControlModeSwitch:
+            buttonControlModeSwitch.isOn = false
+            automaticControlModeSwitch.isOn = false
+
+            writeDataToFirebase("CHEDO", "2") { [weak self] result in
+                switch result {
+                case .success:
+                    self?.handleWriteDataSuccessed(sender.tag)
+                case .failure(let error):
+                    self?.handleWriteDataFailed(error)
+                }
+            }
+        default:
+            buttonControlModeSwitch.isOn = false
+            appControlModeSwitch.isOn = false
+
+            writeDataToFirebase("CHEDO", "3") { [weak self] result in
+                switch result {
+                case .success:
+                    self?.handleWriteDataSuccessed(sender.tag)
+                case .failure(let error):
+                    self?.handleWriteDataFailed(error)
+                }
+            }
+        }
+    }
+
+    private func handleWriteDataSuccessed(_ mode: Any) {
+        let cancelAction = UIAlertAction(title: "Đóng", style: .default)
+
+        showAlert(title: "Thông báo", message: "Chuyển chế độ \(mode) thành công", actions: [cancelAction])
+    }
+
+    private func handleWriteDataFailed(_ error: Error) {
+        print(Self.self, #function, error.localizedDescription)
+
+        let cancelAction = UIAlertAction(title: "Đóng", style: .destructive)
+
+        showAlert(title: "Thông báo", message: "Chuyển chế độ thất bại", actions: [cancelAction])
     }
 }

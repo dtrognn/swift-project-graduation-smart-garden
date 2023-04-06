5d98b5d2f3e2a69827c92bc456c53a9dcbbb5982
Duc Trong <trongvd@heligate.com.vn>
2023-04-06 16:53:42 +0700

    edit func writeDataToFirebase

--- SmartGadrden/SmartGadrden/Common/WriteDataFirebase.swift
+++ SmartGadrden/SmartGadrden/Common/WriteDataFirebase.swift
@@ -5,11 +5,23 @@
 //  Created by Vu Duc Trong on 05/04/2023.
 //
 
-import Foundation
 import FirebaseDatabase
+import Foundation
 
-func writeDataToFirebase(_ ref: String, _ data: Any) {
+// func writeDataToFirebase(_ ref: String, _ data: Any) {
+//    let databaseRef = Database.database().reference().child(ref)
+//
+//    databaseRef.setValue(data)
+// }
+
+func writeDataToFirebase(_ ref: String, _ data: Any, completion: @escaping (Result<Bool, Error>) -> Void) {
     let databaseRef = Database.database().reference().child(ref)
 
-    databaseRef.setValue(data)
+    databaseRef.setValue(data) { error, _ in
+        if let error = error {
+            completion(.failure(error))
+        } else {
+            completion(.success(true))
+        }
+    }
 }

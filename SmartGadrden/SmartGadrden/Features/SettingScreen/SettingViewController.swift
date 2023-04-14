4d7843a6d411fbfdc728c874fc66225e68445ba4
Duc Trong <trongvd@heligate.com.vn>
2023-04-15 01:53:38 +0700

    handle invalid textfield

--- SmartGadrden/SmartGadrden/Features/SettingScreen/SettingViewController.swift
+++ SmartGadrden/SmartGadrden/Features/SettingScreen/SettingViewController.swift
@@ -17,9 +17,12 @@ class SettingViewController: BaseViewController {
     @IBOutlet var appControlModeSwitch: UISwitch!
     @IBOutlet var automaticControlModeSwitch: UISwitch!
 
-    @IBOutlet var thresholdTemperatureTextField: UITextField!
-    @IBOutlet var thresholdSoilMostureTextField: UITextField!
-    @IBOutlet var thresholdLightTextField: UITextField!
+    @IBOutlet var thresholdAboveTempTextField: UITextField!
+    @IBOutlet var thresholdBelowTempTextField: UITextField!
+    @IBOutlet var thresholdAboveSoilMostureTextField: UITextField!
+    @IBOutlet var thresholdBelowSoilMostureTextField: UITextField!
+    @IBOutlet var thresholdAboveLightTextField: UITextField!
+    @IBOutlet var thresholdBelowLightTextField: UITextField!
 
     private var workMode: String = "1"
 
@@ -111,21 +114,22 @@ class SettingViewController: BaseViewController {
             buttonControlModeSwitch.isOn = false
             appControlModeSwitch.isOn = false
 
-            writeDataToFirebase("CHEDO", "3") { [weak self] result in
-                switch result {
-                case .success:
-                    print(Self.self, #function)
-                case .failure(let error):
-                    self?.handleWriteDataFailed(error)
+            if !checkTextFieldsEmpty() {
+                writeDataToFirebase("CHEDO", "3") { [weak self] result in
+                    switch result {
+                    case .success:
+                        self?.writeThresholdTempData()
+                        self?.writeThresholdSoilMostureData()
+                        self?.writeThresholdLightData()
+                    case .failure(let error):
+                        self?.handleWriteDataFailed(error)
+                    }
                 }
+            } else {
+                handleInvalidTextField()
+                initDataModeFirebase()
             }
         }
-    }
-
-    private func handleWriteDataSuccessed(_ mode: Any) {
-        let cancelAction = UIAlertAction(title: "Đóng", style: .default)
-
-        showAlert(title: "Thông báo", message: "Chuyển chế độ \(mode) thành công", actions: [cancelAction])
     }
 
     private func handleWriteDataFailed(_ error: Error) {
@@ -134,5 +138,86 @@ class SettingViewController: BaseViewController {
         let cancelAction = UIAlertAction(title: "Đóng", style: .destructive)
 
         showAlert(title: "Thông báo", message: "Chuyển chế độ thất bại", actions: [cancelAction])
+    }
+
+    // handle case read data textfield
+
+    private func checkTextFieldsEmpty() -> Bool {
+        if automaticControlModeSwitch.isOn {
+            let textFields = [thresholdAboveTempTextField,
+                              thresholdBelowTempTextField,
+                              thresholdAboveSoilMostureTextField,
+                              thresholdBelowSoilMostureTextField,
+                              thresholdAboveLightTextField,
+                              thresholdBelowLightTextField]
+            for textField in textFields {
+                if textField?.text == nil || textField?.text == "" {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    private func writeThresholdTempData() {
+        guard let above = thresholdAboveTempTextField.text,
+              let below = thresholdBelowTempTextField.text else { return }
+
+        let aboveTemp = String(format: "%02d", Int(above) ?? 0)
+        let belowTemp = String(format: "%02d", Int(below) ?? 0)
+
+        let thresholdSoilTemp = aboveTemp + belowTemp
+
+        writeDataToFirebase("thresholdTemp", thresholdSoilTemp) { [weak self] result in
+            switch result {
+            case .success:
+                print(Self.self)
+            case .failure(let error):
+                self?.handleWriteDataFailed(error)
+            }
+        }
+    }
+
+    private func writeThresholdSoilMostureData() {
+        guard let above = thresholdAboveSoilMostureTextField.text,
+              let below = thresholdBelowSoilMostureTextField.text else { return }
+
+        let aboveSoilMoisture = String(format: "%02d", Int(above) ?? 0)
+        let belowSoilMoisture = String(format: "%02d", Int(below) ?? 0)
+
+        let thresholdSoilMoisture = aboveSoilMoisture + belowSoilMoisture
+
+        writeDataToFirebase("thresholdSoilMosture", thresholdSoilMoisture) { [weak self] result in
+            switch result {
+            case .success:
+                print(Self.self)
+            case .failure(let error):
+                self?.handleWriteDataFailed(error)
+            }
+        }
+    }
+
+    private func writeThresholdLightData() {
+        guard let above = thresholdAboveLightTextField.text,
+              let below = thresholdBelowLightTextField.text else { return }
+
+        let aboveLight = String(format: "%02d", Int(above) ?? 0)
+        let belowLight = String(format: "%02d", Int(below) ?? 0)
+
+        let thresholdLight = aboveLight + belowLight
+
+        writeDataToFirebase("thresholdLight", thresholdLight) { [weak self] result in
+            switch result {
+            case .success:
+                print(Self.self)
+            case .failure(let error):
+                self?.handleWriteDataFailed(error)
+            }
+        }
+    }
+
+    private func handleInvalidTextField() {
+        let dismissAction = UIAlertAction(title: "Đóng", style: .default)
+        showAlert(title: "Lỗi", message: "Các thông số ngưỡng không được để trống", actions: [dismissAction])
     }
 }

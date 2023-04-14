bf855769510505b14c278b4bb0e5071153e175ca
Duc Trong <trongvd@heligate.com.vn>
2023-04-14 21:11:03 +0700

    handle screen Temperature lineChart

--- SmartGadrden/SmartGadrden/Features/TempScreen/TemperatureViewController.swift
+++ SmartGadrden/SmartGadrden/Features/TempScreen/TemperatureViewController.swift
@@ -5,14 +5,82 @@
 //  Created by Vu Duc Trong on 12/04/2023.
 //
 
+import Charts
 import UIKit
 
-class TemperatureViewController: UIViewController {
+class TemperatureViewController: BaseViewController {
+    @IBOutlet var lineChart: LineChartView!
+
+    var timeTitle: [String] = []
+    var tempData: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        initDataFirebase()
     }
 
+    private func initDataFirebase() {
+        displayIndicator(isShow: true)
+        fetchDataFromFirebase(atPath: "DULIEUCAMBIEN", dataType: String.self) { [weak self] result in
+            self?.displayIndicator(isShow: false)
+            switch result {
+            case .success(let data):
+                let temp = "\(data.prefix(2))"
+
+                self?.appendTemperature(temp, self!.getCurrentDateTime())
+
+                if self!.tempData.count > 7, self!.timeTitle.count > 7 {
+                    self?.removeFirst()
+                }
+
+                self?.createLineChart(self!.timeTitle, self!.tempData)
+            case .failure(let error):
+                self?.handleReadDataFailed(error)
+            }
+        }
+    }
+
+    private func handleReadDataFailed(_ error: Error) {
+        print("Error: \(error.localizedDescription)")
+
+        let cancelAction = UIAlertAction(title: "Đóng", style: .destructive)
+        showAlert(title: "Lỗi", message: "Lấy dữ liệu không thành công", actions: [cancelAction])
+    }
+
+    private func appendTemperature(_ data: String, _ title: String) {
+        tempData.append(data)
+        timeTitle.append(title)
+    }
+
+    private func removeFirst() {
+        tempData.removeFirst()
+        timeTitle.removeFirst()
+    }
+
+    private func createLineChart(_ timeTitle: [String], _ values: [String]) {
+        var lineArr: [ChartDataEntry] = []
+
+        for i in 0 ..< timeTitle.count {
+            let data = ChartDataEntry(x: Double(i), y: Double(values[i])!)
+            lineArr.append(data)
+        }
+
+        let lineDataSet = LineChartDataSet(entries: lineArr, label: "Temperature")
+        lineDataSet.setColor(.red)
+
+        let xAxis = lineChart.xAxis
+        xAxis.valueFormatter = IndexAxisValueFormatter(values: timeTitle.suffix(7))
+        xAxis.labelCount = 7
+        xAxis.granularity = 1
+
+        xAxis.axisMinimum = -0.3
+        xAxis.axisMaximum = Double(tempData.count) - 0.7
+
+        let lineData = LineChartData(dataSet: lineDataSet)
+        lineChart.data = lineData
+
+        lineChart.animate(xAxisDuration: 2, easingOption: .easeInBounce)
+        lineChart.animate(yAxisDuration: 2, easingOption: .easeInBounce)
+    }
 }

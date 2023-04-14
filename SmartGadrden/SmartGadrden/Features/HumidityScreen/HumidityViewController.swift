11b6eaf8b762b863f72e19f6375e2fa236efff89
Duc Trong <trongvd@heligate.com.vn>
2023-04-14 20:47:09 +0700

    create file manage ViewController

---
+++ SmartGadrden/SmartGadrden/Features/HumidityScreen/HumidityViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HumidityViewController.swift
+//  SmartGadrden
+//
+//  Created by Vu Duc Trong on 12/04/2023.
+//
+
+import UIKit
+
+class HumidityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
